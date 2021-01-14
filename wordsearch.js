d6b2deed6194e45d958af981ebc7e30fbc76f849
Lanuvelza <ianlam1221@hotmail.com>
2021-01-14 01:43:07 +0000

    implemented the function to include the vertical case

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+
+    if (letters === undefined || word === undefined) {
+        return undefined;
+    } else if (letters.length === 0) {
+                return false;
     }
+
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    const vertical = []; 
+    for (let row of letters) {
+        vertical.push([]);
+    }
+
+    for (let i = 0; i < letters.length; i++) {
+        for (let j = 0; j < letters[i].length; j++) {
+            vertical[j].push(letters[i][j]);
+        }
+    }
+    
+    const verticalJoin = vertical.map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+        if(l.includes(word)) {
+            return true;
+        }
+    }
+    
+    return false;
 }
 
 module.exports = wordSearch
